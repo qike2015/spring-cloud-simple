0517428128a0146d27cdf3b5ad07954213c78575
qike2018 <649381130@qq.com>
2018-12-05 11:39:21 +0800

    fix(simple-a)consumer

---
+++ simple-a/simple-a-consumer/src/main/java/com/qike/a/consumer/DetpConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.qike.a.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 豆丁
+ * @since 2018/12/5
+ */
+@SpringBootApplication
+public class DetpConsumerApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DetpConsumerApplication.class, args);
+    }
+
+}
---
+++ simple-a/simple-a-consumer/src/main/java/com/qike/a/consumer/config/ConfigBean.java
@@ -0,0 +1,19 @@
+package com.qike.a.consumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 豆丁
+ * @since 2018/12/5
+ */
+@Configuration
+public class ConfigBean {
+
+    @Bean
+    public RestTemplate getRestTemplate()
+    {
+        return new RestTemplate();
+    }
+}
